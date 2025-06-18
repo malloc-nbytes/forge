2999eb22054eadf42162411523c4a5ba7f5df1f3
malloc-nbytes <zdhdev@yahoo.com>
2025-06-18 01:53:02 -0500

    add module template

--- src/include/forge.h
+++ src/include/forge.h
@@ -3,6 +3,29 @@
 
 #include <stddef.h>
 
+#define FORGE_C_MODULE_TEMPLATE \
+        "#include <forge.h>\n" \
+        "\n" \
+        "char *deps[] = {NULL}; // Must be NULL terminated\n" \
+        "\n" \
+        "char *getname(void) { return \"mypackage\"; }\n" \
+        "char *getver(void) { return \"1.0.0\"; }\n" \
+        "char *getdesc(void) { return \"My Description\"; }\n" \
+        "char **getdeps(void) { return deps; }\n" \
+        "void build(void) {}\n" \
+        "void install(void) {}\n" \
+        "void uninstall(void) {}\n" \
+        "\n" \
+        "FORGE_GLOBAL pkg package = {\n" \
+        "        .name = getname,\n" \
+        "        .ver = getver,\n" \
+        "        .desc = getdesc,\n" \
+        "        .deps = NULL,\n" \
+        "        .build = build,\n" \
+        "        .install = install,\n" \
+        "        .uninstall = uninstall,\n" \
+        "};"
+
 #define FORGE_GLOBAL __attribute__((visibility("default")))
 
 typedef struct {
