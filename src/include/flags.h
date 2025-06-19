93229773a5c3886751dfe6d559d0f2064ae12776
malloc-nbytes <zdhdev@yahoo.com>
2025-06-19 13:13:51 -0500

    add sqlite

--- src/include/flags.h
+++ src/include/flags.h
@@ -1,3 +1,22 @@
+/*
+ * forge: Forge your own packages
+ * Copyright (C) 2025  malloc-nbytes
+ * Contact: zdhdev@yahoo.com
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef FLAGS_H_INCLUDED
 #define FLAGS_H_INCLUDED
 
@@ -13,6 +32,7 @@
 #define FLAG_2HY_NEW "new"
 #define FLAG_2HY_EDIT "edit"
 #define FLAG_2HY_UPDATE "update"
+#define FLAG_2HY_DUMP "dump"
 
 typedef enum { FT_NONE = 1 << 0, } flag_type;
 
