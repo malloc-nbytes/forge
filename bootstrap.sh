d000b281ddac831e81e91aa96c41e91934f9edd3
malloc-nbytes <zdhdev@yahoo.com>
2025-06-19 14:21:46 -0500

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,16 @@
 
 set -xe
 
-wget https://sqlite.org/2025/sqlite-autoconf-3500100.tar.gz
-mv sqlite-autoconf-3500100.tar.gz ./src
-cd src
-tar -vxzf ./sqlite-autoconf-3500100.tar.gz
-cd ..
-autoreconf --install
+if command -v wget >/dev/null 2>&1; then
+    wget https://sqlite.org/2025/sqlite-autoconf-3500100.tar.gz
+    mv sqlite-autoconf-3500100.tar.gz ./src
+    cd src
+    tar -vxzf ./sqlite-autoconf-3500100.tar.gz
+    cd ..
+    autoreconf --install
+else
+    unset -x
+    echo "wget is not installed, it is required to get sqlite."
+    echo "You could also download it by visiting this link: https://sqlite.org/2025/sqlite-autoconf-3500100.tar.gz"
+fi
+
