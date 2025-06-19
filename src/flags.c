d20f06af3158245d12eb386046f823d8aeb18ea3
malloc-nbytes <zdhdev@yahoo.com>
2025-06-19 14:09:06 -0500

    update usage

--- src/flags.c
+++ src/flags.c
@@ -22,6 +22,7 @@
 
 #include "flags.h"
 #include "colors.h"
+#include "config.h"
 
 void
 usage(void)
@@ -31,18 +32,30 @@ usage(void)
         printf("This is free software, and you are welcome to redistribute it\n");
         printf("under certain conditions; see --copying\n\n");
 
+        printf("Compilation Information:\n");
+        printf("| cc: " COMPILER_NAME "\n");
+        printf("| path: " COMPILER_PATH "\n");
+        printf("| ver.: " COMPILER_VERSION "\n");
+        printf("| flags: " COMPILER_FLAGS "\n\n");
+
+        printf("Github repository: https://www.github.com/malloc-nbytes/forge.git/\n\n");
+        printf("Send bug reports to:\n");
+        printf("  - https://github.com/malloc-nbytes/forge/issues\n");
+        printf("  - or " PACKAGE_BUGREPORT "\n\n");
+
         printf("Usage: forge " YELLOW BOLD "[options...] " RESET GREEN BOLD "<command>" RESET " [arguments...]\n");
         printf("Options:\n");
-        printf(YELLOW BOLD "    -%c, --%s" RESET "          display this message\n", FLAG_1HY_HELP, FLAG_2HY_HELP);
-        printf(YELLOW BOLD "    -%c, --%s" RESET "       rebuild package modules\n", FLAG_1HY_REBUILD, FLAG_2HY_REBUILD);
+        printf(YELLOW BOLD "    -%c, --%s" RESET "            display this message\n", FLAG_1HY_HELP, FLAG_2HY_HELP);
+        printf(YELLOW BOLD "    -%c, --%s       R"                         RESET  " rebuild package modules\n", FLAG_1HY_REBUILD, FLAG_2HY_REBUILD);
         printf("Commands:\n");
-        printf(GREEN BOLD "    %s          " RESET "      list installed packages\n", FLAG_2HY_LIST);
-        printf(GREEN BOLD "    %s <pkg...> " RESET "   install packages\n", FLAG_2HY_INSTALL);
-        printf(GREEN BOLD "    %s <pkg...> " RESET " uninstall packages\n", FLAG_2HY_UNINSTALL);
-        printf(GREEN BOLD "    %s <pkg...> " RESET "    update packages or leave empty to update all\n", FLAG_2HY_UPDATE);
-        printf(GREEN BOLD "    %s <pkg>    " RESET "      list dependencies of `pkg`\n", FLAG_2HY_DEPS);
-        printf(GREEN BOLD "    %s <name>   " RESET "       create a new package module\n", FLAG_2HY_NEW);
-        printf(GREEN BOLD "    %s <name>   " RESET "      edit an existing package module\n", FLAG_2HY_EDIT);
-        printf(GREEN BOLD "    %s <name...>" RESET "      dump a package module\n", FLAG_2HY_DUMP);
+        printf(GREEN BOLD "    %s          " RESET                                "        list installed packages\n", FLAG_2HY_LIST);
+        printf(GREEN BOLD "    %s <pkg...> " RESET YELLOW BOLD "   R "     RESET  "install packages\n", FLAG_2HY_INSTALL);
+        printf(GREEN BOLD "    %s <pkg...> " RESET YELLOW BOLD " R "       RESET  "uninstall packages\n", FLAG_2HY_UNINSTALL);
+        printf(GREEN BOLD "    %s <pkg...> " RESET YELLOW BOLD "    R "    RESET  "update packages or leave empty to update all\n", FLAG_2HY_UPDATE);
+        printf(GREEN BOLD "    %s <pkg>    " RESET                                "        list dependencies of `pkg`\n", FLAG_2HY_DEPS);
+        printf(GREEN BOLD "    %s <name>   " RESET YELLOW BOLD "       R " RESET  "create a new package module\n", FLAG_2HY_NEW);
+        printf(GREEN BOLD "    %s <name>   " RESET YELLOW BOLD "      R "  RESET  "edit an existing package module\n", FLAG_2HY_EDIT);
+        printf(GREEN BOLD "    %s <name...>" RESET                                "        dump a package module\n", FLAG_2HY_DUMP);
+        printf("Note: " YELLOW BOLD "R" RESET " requires root permissions\n");
         exit(0);
 }
