4e6ca81a1168277f81dcdfa62fccdee7ddafd2b9
malloc-nbytes <zdhdev@yahoo.com>
2025-06-21 13:41:43 -0500

    add forge_smap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,15 @@
 set -xe
 
 if command -v wget >/dev/null 2>&1; then
-    wget https://sqlite.org/2025/sqlite-autoconf-3500100.tar.gz
-    mv sqlite-autoconf-3500100.tar.gz ./src
-    cd src
-    tar -vxzf ./sqlite-autoconf-3500100.tar.gz
-    cd ..
+    if [ ! -e "src/sqlite-autoconf-3500100" ]; then
+        wget https://sqlite.org/2025/sqlite-autoconf-3500100.tar.gz
+        mv sqlite-autoconf-3500100.tar.gz ./src
+        cd src
+        tar -vxzf ./sqlite-autoconf-3500100.tar.gz
+        cd ..
+    fi
     autoreconf --install
+    ./configure --prefix=/usr --libdir=/usr/lib64
 else
     unset -x
     echo "wget is not installed, it is required to get sqlite."
---
+++ src/forge/smap.h
@@ -0,0 +1,27 @@
+#ifndef SMAP_H_INCLUDED
+#define SMAP_H_INCLUDED
+
+#include <stddef.h>
+
+#define FORGE_SMAP_DEFAULT_TBL_CAPACITY 256
+
+typedef struct __forge_smap_node {
+        char *k; // owns the string
+        void *v;
+        struct __forge_smap_node *n;
+} __forge_smap_node;
+
+typedef struct {
+        __forge_smap_node **tbl;
+        size_t len; // number of table entries
+        size_t cap; // capacity of table
+        size_t sz; // how many total nodes
+} forge_smap;
+
+forge_smap forge_smap_create(void);
+void forge_smap_insert(forge_smap *map, const char *k, void *v);
+int forge_smap_contains(const forge_smap *map, const char *k);
+void *forge_smap_get(const forge_smap *map, const char *k);
+void forge_smap_destroy(forge_smap *map);
+
+#endif // SMAP_H_INCLUDED
