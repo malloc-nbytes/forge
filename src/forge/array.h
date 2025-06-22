f43f4ba5cbc09a8ff8e37b7720969df2a5a03340
malloc-nbytes <zdhdev@yahoo.com>
2025-06-22 15:57:05 -0500

    doc array.h and cmd.h

--- src/forge/array.h
+++ src/forge/array.h
@@ -25,19 +25,20 @@
 /* This file is used for making stack alloc'd dynamic
  * arrays where we dont need to use the Array<T> DS. */
 
-//////////////////////////////////////////////////
-// Creates a new dynamic array type globally.
-// Note: Use dyn_array_init() to initialize
-//       any instances of it.
-// Example:
-//   dyn_array_type(int, Int_Array);
-//
-//   void f(Int_Array *arr);
-//
-//   int main(void) {
-//       Int_Array arr;
-//       return 0;
-//   }
+/**
+ * Creates a new dynamic array type globally.
+ * Note: Use dyn_array_init() to initialize
+ *       any instances of it.
+ * Example:
+ *   dyn_array_type(int, Int_Array);
+ *
+ *   void f(Int_Array *arr);
+ *
+ *   int main(void) {
+ *       Int_Array arr;
+ *       return 0;
+ *   }
+ */
 #define DYN_ARRAY_TYPE(ty, name) \
     typedef struct {             \
         ty *data;                \
@@ -51,17 +52,18 @@
                 .cap = 0,                       \
         }
 
-//////////////////////////////////////////////////
-// Initializes a global array type. This is only
-// used if you use DYN_ARRAY_TYPE().
-// Example:
-//   dyn_array_type(int, Int_Array);
-//
-//   int main(void) {
-//       Int_Array arr;
-//       dyn_array_init_type(arr); // <- here
-//       return 0;
-//   }
+/**
+ * Initializes a global array type. This is only
+ * used if you use DYN_ARRAY_TYPE().
+ * Example:
+ *   dyn_array_type(int, Int_Array);
+ *
+ *   int main(void) {
+ *       Int_Array arr;
+ *       dyn_array_init_type(arr); // <- here
+ *       return 0;
+ *   }
+ */
 #define dyn_array_init_type(da)                 \
     do {                                        \
         (da).data = malloc(sizeof(*(da).data)); \
@@ -69,22 +71,24 @@
         (da).len = 0;                           \
     } while (0)
 
-//////////////////////////////////////////////////
-// Creates a new dynamic array on the stack.
-// Example:
-//   dyn_array(int, int_vector);
+/**
+ * Creates a new dynamic array on the stack.
+ * Example:
+ *   dyn_array(int, int_vector);
+ */
 #define dyn_array(ty, name)                                        \
     struct {                                                       \
         ty *data;                                                  \
         size_t len, cap;                                           \
     } (name) = { .data = (typeof(ty) *)malloc(sizeof(ty)), .len = 0, .cap = 1 };
 
-//////////////////////////////////////////////////
-// Append to a dynamic array.
-// Example:
-//   dyn_array(int, int_vector);
-//   for (int i = 0; i < 10; ++i)
-//     dyn_array_append(int_vector, i);
+/**
+ * Append to a dynamic array.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   for (int i = 0; i < 10; ++i)
+ *     dyn_array_append(int_vector, i);
+ */
 #define dyn_array_append(da, value)                                     \
     do {                                                                \
         if ((da).len >= (da).cap) {                                     \
@@ -96,11 +100,12 @@
         (da).data[(da).len++] = (value);                                \
     } while (0)
 
-//////////////////////////////////////////////////
-// Free a dynamic array.
-// Example:
-//   dyn_array(int, int_vector);
-//   dyn_array_free(int_vector);
+/**
+ * Free a dynamic array.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   dyn_array_free(int_vector);
+*/
 #define dyn_array_free(da)       \
     do {                         \
         if ((da).data != NULL) { \
@@ -109,42 +114,46 @@
         (da).len = (da).cap = 0; \
     } while (0)
 
-//////////////////////////////////////////////////
-// Get an element safely at an index.
-// Will panic if the element is out of bounds.
-// Example:
-//   dyn_array(int, int_vector);
-//   dyn_array_append(int_vector, i);
-//   printf("%d\n", dyn_array_at_s(int_vector));
+/**
+ * Get an element safely at an index.
+ * Will panic if the element is out of bounds.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   dyn_array_append(int_vector, i);
+ *   printf("%d\n", dyn_array_at_s(int_vector));
+ */
 #define dyn_array_at_s(da, i)                                      \
     ((i) < (da).len ? (da).data[i] : (fprintf(stderr,              \
     "[dyn_array error]: index %zu is out of bounds (len = %zu)\n", \
     (size_t)(i), (size_t)(da).len), exit(1), (da).data[0]))
 
-//////////////////////////////////////////////////
-// Get an element at an index.
-// Example:
-//   dyn_array(int, int_vector);
-//   dyn_array_append(int_vector, i);
-//   printf("%d\n", dyn_array_at(int_vector));
+/**
+ * Get an element at an index.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   dyn_array_append(int_vector, i);
+ *   printf("%d\n", dyn_array_at(int_vector));
+ */
 #define dyn_array_at(da, i) ((da).data[i])
 
-//////////////////////////////////////////////////
-// Clear a dynamic array.
-// Example:
-//   dyn_array(int, int_vector);
-//   dyn_array_append(int_vector, i);
-//   dyn_array_clear(int_vector);
+/**
+ * Clear a dynamic array.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   dyn_array_append(int_vector, i);
+ *   dyn_array_clear(int_vector);
+ */
 #define dyn_array_clear(da) (da).len = 0;
 
-//////////////////////////////////////////////////
-// Remove an element at index `idx`.
-// Example:
-//   dyn_array(int, int_vector);
-//   ...
-//   dyn_array_rm_at(int_vector, 0);
-//   dyn_array_rm_at(int_vector, 5);
-//   ...
+/**
+ * Remove an element at index `idx`.
+ * Example:
+ *   dyn_array(int, int_vector);
+ *   ...
+ *   dyn_array_rm_at(int_vector, 0);
+ *   dyn_array_rm_at(int_vector, 5);
+ *   ...
+ */
 #define dyn_array_rm_at(da, idx) \
     do {                                                     \
         for (size_t __i_ = (idx); __i_ < (da).len-1; ++__i_) \
@@ -155,6 +164,8 @@
 #define dyn_array_explode(da) (da).data, (da).len, (da).cap
 
 #define dyn_array_explode_mem(da) &(da).data, &(da).len, &(da).cap
+
+// Common types for arrays.
 
 DYN_ARRAY_TYPE(int,      int_array);
 DYN_ARRAY_TYPE(char,     char_array);
--- src/forge/cmd.h
+++ src/forge/cmd.h
@@ -1,15 +1,89 @@
 #ifndef CMD_H_INCLUDED
 #define CMD_H_INCLUDED
 
+/**
+ * Parameter: fp -> the filepath to cd into
+ * Returns: 1 on success, 0 on failure
+ * Description: cd into the filepath `fp`.
+ */
 int cd(const char *fp);
+
+/**
+ * Parameter: fp -> the filepath to cd into
+ * Returns: 1 on success, 0 on failure
+ * Description: the same as cd(), but silent.
+ */
 int cd_silent(const char *fp);
+
+/**
+ * Parameter: cmd -> the command to execute
+ * Returns: 1 on success, 0 on failure
+ * Description: Issue a BASH command.
+ */
 int cmd(const char *cmd);
+
+/**
+ * Parameter: cmd -> the command to execute
+ * Parameter: username -> the user to execute the command as
+ * Returns: 1 on success, 0 on failure
+ * Description: Issue a BASH command as a specific user.
+ *              This is useful if the program is being ran
+ *              through `sudo` and you need to create some
+ *              files not in /root/. It might be useful to
+ *              call get_prev_user() to get the user that
+ *              ran forge through sudo.
+ */
 int cmd_as(const char *cmd, const char *username);
+
+/**
+ * Parameter: cmd -> the command to execute
+ * Returns: the output of the command, or NULL on failure
+ * Description: Issue a BASH command and capture the output.
+ *              If the command fails or something goes wrong,
+ *              return return result will be NULL.
+ */
 char *cmdout(const char *cmd);
+
+/**
+ * Parameter: author -> the author of the program
+ * Parameter: name -> the name of the program
+ * Returns: the name of the program
+ * Description: Do a `git clone https://www.github.com/<author>/<name>.git`.
+ *              This function returns the name of the command as it is
+ *              convenient for the download() function in the C modules.
+ */
 char *git_clone(char *author, char *name);
+
+/**
+ * Parameter: fp -> the filepath to create
+ * Returns: the filepath, or NULL on failure
+ * Description: Create a directory with the `-p` flag.
+ */
 char *mkdirp(char *fp);
+
+/**
+ * Parameter: var -> the environment variable
+ * Returns: the value of the environment variable, or NULL on failure
+ * Description: Get the value of an environment variable. Do not
+ *              include the dollarsign ($) in the variable name.
+ */
 char *env(const char *var);
+
+/**
+ * Returns: the username of the previous user
+ * Description: Get the username of the previous user
+ *              calling forge. For example, if forge was
+ *              ran as: `USER@/bin/sh# sudo forge install ...`, then the
+ *              result will be USER.
+ */
 char *get_prev_user(void);
+
+/**
+ * Parameter: path -> the path to the file to change
+ * Parameter: user -> the user to give ownership to
+ * Returns: 1 on success, or 0 on failure
+ * Description: Change the ownership of `path` to `user`.
+ */
 int change_file_owner(const char *path, const char *user);
 
 #endif // CMD_H_INCLUDED
