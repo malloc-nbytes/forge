f5d4faae146ce04dc1e4ef19449e73ce805c6aab
malloc-nbytes <zdhdev@yahoo.com>
2025-06-25 15:06:38 -0500

    update docs

--- src/forge/conf.h
+++ src/forge/conf.h
@@ -1,8 +1,13 @@
 #ifndef CONF_H_INCLUDED
 #define CONF_H_INCLUDED
 
+// How many j flags to pass to make
 #define FORGE_PREFERRED_MAKEFILE_JFLAGS "$(nproc)"
+
+// Where to install to
 #define FORGE_PREFERRED_INSTALL_PREFIX "/usr/local"
+
+// Where to install libraries to
 #define FORGE_PREFERRED_LIB_PREFIX "/usr/local/lib64"
 
 #endif // CONF_H_INCLUDED
--- src/forge/smap.h
+++ src/forge/smap.h
@@ -11,6 +11,7 @@ typedef struct __forge_smap_node {
         struct __forge_smap_node *n;
 } __forge_smap_node;
 
+// A string map provided by forge
 typedef struct {
         __forge_smap_node **tbl;
         size_t len; // number of table entries
@@ -18,12 +19,58 @@ typedef struct {
         size_t sz; // how many total nodes
 } forge_smap;
 
+/**
+ * Returns: a new string map
+ * Description: Create a new string map. Make sure to
+ *              call forge_smap_destroy() to free memory.
+ */
 forge_smap forge_smap_create(void);
+
+/**
+ * Parameter: map -> the map to insert to
+ * Parameter: k   -> the key
+ * Parameter: v   -> the value
+ * Description: Insert key `k` with value `v` into the map `map`.
+ */
 void forge_smap_insert(forge_smap *map, const char *k, void *v);
+
+/**
+ * Parameter: map -> the map to query
+ * Parameter: k   -> the key to check
+ * Returns: 1 if found, 0 if otherwise
+ * Description: See if the key `k` is inside of the map `map`.
+ */
 int forge_smap_contains(const forge_smap *map, const char *k);
+
+/**
+ * Parameter: map -> the map to get from
+ * Parameter: k   -> the key with the associated value
+ * Returns: the value of of the key `k`.
+ * Description: Get the value that the key `k` is associated with.
+ */
 void *forge_smap_get(const forge_smap *map, const char *k);
+
+/**
+ * Parameter: map -> the map to destroy
+ * Description: free()'s all memory that `map` allocates.
+ */
 void forge_smap_destroy(forge_smap *map);
+
+/**
+ * Parameter: map -> the map to iterate
+ * Returns: an array of keys (NULL terminated, NULL on failure)
+ * Description: Use this function to get all keys inside of
+ *              the map. It is guaranteed to be NULL terminated.
+ *              The result needs to be free()'d. The individual strings
+ *              do not need to be free()'d.
+ */
 char **smap_iter(const forge_smap *map);
+
+/**
+ * Paramter: map -> the map to get the size from
+ * Returns: the number of nodes in the map
+ * Description: Get the number of nodes stored inside of `map`.
+ */
 size_t forge_smap_size(const forge_smap *map);
 
 #endif // SMAP_H_INCLUDED
