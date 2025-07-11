b59b055a7de188d3f93143bf9e8681001e67c4dc
malloc-nbytes <zdhdev@yahoo.com>
2025-07-10 21:58:58 -0500

    start on help functions

--- src/flags.c
+++ src/flags.c
@@ -19,11 +19,203 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "flags.h"
 #include "colors.h"
 #include "config.h"
 #include "copying.h"
+#include "utils.h"
+
+static void
+help_help(void)
+{
+}
+
+static void
+help_rebuild(void)
+{
+}
+
+static void
+help_sync(void)
+{
+}
+
+static void
+help_list(void)
+{
+}
+
+static void
+help_search(void)
+{
+}
+
+static void
+help_install(void)
+{
+}
+
+static void
+help_uninstall(void)
+{
+}
+
+static void
+help_update(void)
+{
+}
+
+static void
+help_add_repo(void)
+{
+}
+
+static void
+help_drop_repo(void)
+{
+}
+
+static void
+help_list_repos(void)
+{
+}
+
+static void
+help_deps(void)
+{
+}
+
+static void
+help_new(void)
+{
+}
+
+static void
+help_edit(void)
+{
+}
+
+static void
+help_dump(void)
+{
+}
+
+static void
+help_drop(void)
+{
+}
+
+static void
+help_files(void)
+{
+}
+
+static void
+help_api(void)
+{
+}
+
+static void
+help_restore(void)
+{
+}
+
+static void
+help_copying(void)
+{
+}
+
+static void
+help_depgraph(void)
+{
+}
+
+static void
+help_apilist(void)
+{
+}
+
+static void
+help_editconf(void)
+{
+}
+
+static void
+help_updateforge(void)
+{
+}
+
+void
+help(const char *flag)
+{
+        void (*hs[])(void) = {
+                help_help,
+                help_rebuild,
+                help_sync,
+                help_list,
+                help_search,
+                help_install,
+                help_uninstall,
+                help_update,
+                help_add_repo,
+                help_drop_repo,
+                help_list_repos,
+                help_deps,
+                help_new,
+                help_edit,
+                help_dump,
+                help_drop,
+                help_files,
+                help_api,
+                help_restore,
+                help_copying,
+                help_depgraph,
+                help_apilist,
+                help_editconf,
+                help_updateforge,
+        };
+
+        size_t n = strlen(flag);
+
+        if (n == 2 && flag[0] == '-' && flag[1] == FLAG_1HY_HELP) {
+        } else if (n == 1 && flag[0] == '-' && flag[1] == FLAG_1HY_REBUILD) {
+        } else if (n == 1 && flag[0] == '-' && flag[1] == FLAG_1HY_SYNC) {
+        }
+        else if (!strcmp(flag, FLAG_2HY_HELP)) {}
+        else if (!strcmp(flag, FLAG_2HY_LIST)) {}
+        else if (!strcmp(flag, FLAG_2HY_DEPS)) {}
+        else if (!strcmp(flag, FLAG_2HY_INSTALL)) {}
+        else if (!strcmp(flag, FLAG_2HY_UNINSTALL)) {}
+        else if (!strcmp(flag, FLAG_2HY_REBUILD)) {}
+        else if (!strcmp(flag, FLAG_2HY_NEW)) {}
+        else if (!strcmp(flag, FLAG_2HY_EDIT)) {}
+        else if (!strcmp(flag, FLAG_2HY_UPDATE)) {}
+        else if (!strcmp(flag, FLAG_2HY_DUMP)) {}
+        else if (!strcmp(flag, FLAG_2HY_SYNC)) {}
+        else if (!strcmp(flag, FLAG_2HY_DROP_BROKEN_PKGS)) {}
+        else if (!strcmp(flag, FLAG_2HY_DROP)) {}
+        else if (!strcmp(flag, FLAG_2HY_FILES)) {}
+        else if (!strcmp(flag, FLAG_2HY_COPYING)) {}
+        else if (!strcmp(flag, FLAG_2HY_DEPGRAPH)) {}
+        else if (!strcmp(flag, FLAG_2HY_API)) {}
+        else if (!strcmp(flag, FLAG_2HY_EDITCONF)) {}
+        else if (!strcmp(flag, FLAG_2HY_UPDATEFORGE)) {}
+        else if (!strcmp(flag, FLAG_2HY_RESTORE)) {}
+        else if (!strcmp(flag, FLAG_2HY_APILIST)) {}
+        else if (!strcmp(flag, FLAG_2HY_SEARCH)) {}
+        else if (!strcmp(flag, FLAG_2HY_ADD_REPO)) {}
+        else if (!strcmp(flag, FLAG_2HY_DROP_REPO)) {}
+        else if (!strcmp(flag, FLAG_2HY_LIST_REPOS)) {}
+        else if (!strcmp(flag, "*")) {
+                for (size_t i = 0; i < sizeof(hs)/sizeof(*hs); ++i) {
+                        hs[i]();
+                }
+        } else {
+                err_wargs("unknown flag `%s`", flag);
+        }
+}
 
 void
 usage(void)
@@ -50,7 +242,7 @@ usage(void)
 
         printf("Usage: forge " YELLOW BOLD "[options...] " RESET GREEN BOLD "<command>" RESET " [arguments...]\n");
         printf("Options:\n");
-        printf(YELLOW BOLD "    -%c, --%s" RESET "                display this message\n", FLAG_1HY_HELP, FLAG_2HY_HELP);
+        printf(YELLOW BOLD "    -%c, --%s[=<flag>|*]" RESET "     display this message or view help on a command or option\n", FLAG_1HY_HELP, FLAG_2HY_HELP);
         printf(YELLOW BOLD "    -%c, --%s           R"                         RESET  " rebuild package modules\n", FLAG_1HY_REBUILD, FLAG_2HY_REBUILD);
         printf(YELLOW BOLD "    -%c, --%s              R"                         RESET " sync the C modules repository\n", FLAG_1HY_SYNC, FLAG_2HY_SYNC);
         printf(YELLOW BOLD "        --%s  R"                         RESET " remove all broken packages\n", FLAG_2HY_DROP_BROKEN_PKGS);
